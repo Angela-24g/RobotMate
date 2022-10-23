367fbc60be2bfc55bf6ed61e9880ffd238f76df8
bdgom <bdgom73@naver.com>
2022-10-23 20:03:10 +0900

    RM001 모듈 추가
    - core 모듈 설정
    - domain 모듈 설정
    - logback 설정

--- module-home/src/main/java/com/project/robotmate/home/RobotmateHomeApplication.java
+++ module-home/src/main/java/com/project/robotmate/home/RobotmateHomeApplication.java
@@ -1,4 +1,4 @@
-package com.project.robotmate;
+package com.project.robotmate.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
