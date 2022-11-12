4406f39f5e468d8dc8afea602eced18c62952aa5
iljoong <iljoong.kim@unlike.kr>
2022-11-12 16:12:57 +0900

    RM-004 jsp 설정

--- module-home/src/main/java/com/project/robotmate/home/controller/HomeController.java
+++ module-home/src/main/java/com/project/robotmate/home/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.project.robotmate.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/hello")
+    public String home2() {
+        return "hello";
     }
 }
