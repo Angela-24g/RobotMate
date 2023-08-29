616afe6936eb5113b9db562eb52d37c1895d780a
kij <bdgom73@naver.com>
2023-08-29 17:24:47 +0900

    fix: 관리자 갤러리 편집 버그 수정

--- module-admin/src/main/resources/static/js/gallery/gallery.edit.js
+++ module-admin/src/main/resources/static/js/gallery/gallery.edit.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
     const galleryForm = document.getElementById("gallery-edit-form");
     const gallerySubmitButton = document.getElementById("gallery-button");
